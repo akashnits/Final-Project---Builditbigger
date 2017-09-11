1877aa1d9df1ce65e31df4641932b13b47dbf83b
akash.nits <akash.nits@outlook.com>
2017-09-12 01:02:57 +0530

    Test fixed

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,10 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
-import com.udacity.gradle.builditbigger.paid.MainActivity;
 
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,13 +17,15 @@ import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 
+
+
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
+
         @Test
         public void testDoInBackground() throws Exception{
             try {
-                MainActivity mainActivity = new MainActivity();
-                EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(mainActivity);
+                EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(InstrumentationRegistry.getTargetContext());
                 endpointsAsyncTask.execute();
                 String result = endpointsAsyncTask.get(30, TimeUnit.SECONDS);
 
